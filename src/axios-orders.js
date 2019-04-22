10bb29eb1fc8f043a360ed13c363230a796398f4
Cancain <eriksson.tomas@gmail.com>
2019-04-22 12:51:45 +0200

    fixed link to backend

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://react-my-burger.firebaseio.com/'
+  baseURL: "https://react-my-burger-f3de7.firebaseio.com"
 });
 
 export default instance;
